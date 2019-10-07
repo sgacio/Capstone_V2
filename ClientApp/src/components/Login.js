04b5145e6e7d73aeb9f16c9279dfc9b140b86ef2
ads6495 <astrange55@icloud.com>
2019-10-07 13:05:47 -0400

    added post with auth

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState } from 'react'
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import config from "./FirebaseAuth";
-
-
+import Axios from 'axios';
 
 const Login = () => {
+  const [userData, setUserData] = useState(result)
+
+
   firebase.initializeApp(config);
   const firebaseAppAuth = firebase.auth();
 
@@ -19,9 +22,16 @@ const Login = () => {
     })
   }
 
+  const putUserOnServer = async () => {
+    const resp = await Axios.post('https://localhost:5001/api/PlayerStat', {
+      ProperName: userData.displayName,
+      Email: userData.email
+    })
+  }
+
   return (
     <div>
-      <button onClick={() => signInWithPopup()} type="">
+      <button onClick={() => signInWithPopup(), putUserOnServer()} type="">
         Sign in
       </button>
     </div>
