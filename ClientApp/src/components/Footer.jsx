85caf835d9e7c3574379c3edd28bdf69a1cfd1eb
Stephen Gacio <sgacio@mail.usf.edu>
2019-10-16 12:05:43 -0400

    css changes

--- ClientApp/src/components/Footer.jsx
+++ ClientApp/src/components/Footer.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 const Footer = () => {
   return (
     <div>
       <footer class="page-footer font-small blue">
-
-
-        <div class="footer-copyright text-center py-3">© 2019 Copyright:
-<a target="_blank" href="https://www.suncoast.io"> made with &hearts; at SDG in st.petersburg,Fl.</a>
+        <div class="footer-copyright text-center py-3">
+          © 2019 Copyright:
+          <a target="_blank" href="https://www.suncoast.io">
+            {' '}
+            made with &hearts; at SDG in st.petersburg,Fl.
+          </a>
         </div>
-
       </footer>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
